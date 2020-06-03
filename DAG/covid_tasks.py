315069d74c66c1b9d9d9006210b8a791edb2dcfb
Aditi <aditi@nineleaps.com>
2020-06-03 16:37:15 +0530

    Covid Airflow Project

---
+++ DAG/covid_tasks.py
@@ -0,0 +1,90 @@
+#Importing modules
+from datetime import timedelta, date
+import urllib.request as request
+import json
+import pandas as pd
+import airflow
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from bigQueryTest import BigQuery
+
+#Default arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(0),
+    # 'email': ['airflow@example.com'],
+    # 'email_on_failure': False,
+    # 'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+#Instantiate a DAG
+dag = DAG(
+    'covid_tasks', default_args=default_args,
+    schedule_interval="@daily")
+
+today = date.today().strftime("%Y-%d-%m")
+globals()['saved_record_length'] = 0
+globals()['uploaded_record_length'] = 0
+
+#Tasks
+def get_statewise_data():
+
+
+    with request.urlopen('https://api.covid19india.org/data.json') as response:
+        source = response.read()
+        data = json.loads(source)
+
+    statewise_dict = data['statewise']
+
+    df = pd.DataFrame(statewise_dict, columns=['state', 'active'])
+    date = []
+    saved_record_length = len(df.index)
+    for i in range(saved_record_length):
+        date.append(today)
+    df['date'] = date
+
+    df.to_csv(f'/mnt/c/plugins/Output/CovidStats({today}).csv', index=False)
+    saved_record_length_data = [[saved_record_length]]
+    df2 = pd.DataFrame(saved_record_length_data, columns=["records"])
+    df2.to_csv('/mnt/c/plugins/Output/saved_record_length.csv', index=False)
+
+
+def upload_data():
+    uploaded_record_length = BigQuery().load_data_into_bqtable(today)
+    uploaded_record_length_data = [[uploaded_record_length]]
+    df = pd.DataFrame(uploaded_record_length_data, columns=["records"])
+    df.to_csv('/mnt/c/plugins/Output/uploaded_record_length.csv', index=False)
+
+def upload_status():
+    df = pd.read_csv('/mnt/c/plugins/Output/saved_record_length.csv')
+    saved_record_length = df.at[0, 'records']
+
+    df2 = pd.read_csv('/mnt/c/plugins/Output/uploaded_record_length.csv')
+    uploaded_record_length = df2.at[0, 'records']
+
+    percentage = uploaded_record_length / saved_record_length * 100
+    data = [[today, percentage]]
+    df = pd.DataFrame(data, columns=["Date", "Percentage of Upload"])
+    df.to_csv('/mnt/c/plugins/Output/UploadPercentageStatus.csv', index=False)
+
+    BigQuery().load_status_into_bq_table()
+
+
+#Task1
+get_data_operator = PythonOperator(task_id='get_covid_data_task', python_callable=get_statewise_data, dag=dag)
+
+#Task2
+upload_data_operator = PythonOperator(task_id='upload_covid_data_task', python_callable=upload_data, dag=dag)
+
+#Task3
+upload_status_operator = PythonOperator(task_id="upload_status_task", python_callable=upload_status, dag=dag)
+
+#Setting up dependencies
+get_data_operator >> upload_data_operator >> upload_status_operator
---
+++ plugins/bigQueryTest.py
@@ -0,0 +1,101 @@
+from google.api_core.exceptions import NotFound
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+
+class BigQuery:
+    def load_data_into_bqtable(self, date):
+        path = '/mnt/c/plugins/Covid-2f95220d7160.json'
+        # creating credentials
+        credentials = service_account.Credentials.from_service_account_file(
+            path,
+            scopes=["https://www.googleapis.com/auth/cloud-platform"]
+        )
+        # creating client
+        client = bigquery.Client(
+            credentials=credentials,
+            project=credentials.project_id)
+        dataset_id = "Aditi"
+        dataset_ref = client.dataset(dataset_id)
+        # creating dataset if not exists
+        try:
+            client.get_dataset(dataset_ref)
+        except NotFound:
+            dataset = bigquery.Dataset(dataset_ref)
+            dataset = client.create_dataset(dataset)
+            print('Dataset {} created.'.format(dataset.dataset_id))
+        job_config = bigquery.LoadJobConfig()
+        # autodetect schema from the given csv
+        job_config.autodetect = True
+        # only create if table not exists
+        job_config.create_disposition = 'CREATE_IF_NEEDED'
+        job_config.skip_leading_rows = 1
+        # The source format defaults to CSV, so the line below is optional.
+        job_config.source_format = bigquery.SourceFormat.CSV
+        uri = f"/mnt/c/plugins/Output/CovidStats({date}).csv"
+        with open(uri, "rb") as source_file:
+            load_job = client.load_table_from_file(
+                source_file, dataset_ref.table("covid_data"), job_config=job_config
+            )
+        # API request
+        print("Starting job {}".format(load_job.job_id))
+        load_job.result()  # Waits for table load to complete.
+        print("Job finished.")
+        assert load_job.state == 'DONE'
+        destination_table = client.get_table(dataset_ref.table("covid_data"))
+        print("Loaded {} rows.".format(destination_table.num_rows))
+        query = ('SELECT * FROM `{}.{}` WHERE date = "{}"'
+                 .format(dataset_id, 'covid_data', date))
+        try:
+            query_job = client.query(query)
+            results = query_job.result()
+            row_count = 0
+            for row in results:
+                row_count += 1
+
+        except Exception as e:
+            print("Error")
+            print(e)
+
+        return row_count
+
+    def load_status_into_bq_table(self):
+        path = '/mnt/c/plugins/Covid-2f95220d7160.json'
+        # creating credentials
+        credentials = service_account.Credentials.from_service_account_file(
+            path,
+            scopes=["https://www.googleapis.com/auth/cloud-platform"]
+        )
+        # creating client
+        client = bigquery.Client(
+            credentials=credentials,
+            project=credentials.project_id)
+        dataset_id = "Aditi"
+        dataset_ref = client.dataset(dataset_id)
+        # creating dataset if not exists
+        try:
+            client.get_dataset(dataset_ref)
+        except NotFound:
+            dataset = bigquery.Dataset(dataset_ref)
+            dataset = client.create_dataset(dataset)
+            print('Dataset {} created.'.format(dataset.dataset_id))
+        job_config = bigquery.LoadJobConfig()
+        # autodetect schema from the given csv
+        job_config.autodetect = True
+        # only create if table not exists
+        job_config.create_disposition = 'CREATE_IF_NEEDED'
+        job_config.skip_leading_rows = 1
+        # The source format defaults to CSV, so the line below is optional.
+        job_config.source_format = bigquery.SourceFormat.CSV
+        uri = f"/mnt/c/plugins/Output/UploadPercentageStatus.csv"
+        with open(uri, "rb") as source_file:
+            load_job = client.load_table_from_file(
+                source_file, dataset_ref.table("upload_status"), job_config=job_config
+            )
+        # API request
+        print("Starting job {}".format(load_job.job_id))
+        load_job.result()  # Waits for table load to complete.
+        print("Job finished.")
+        assert load_job.state == 'DONE'
+        destination_table = client.get_table(dataset_ref.table("upload_status"))
+        print("Loaded {} rows.".format(destination_table.num_rows))
